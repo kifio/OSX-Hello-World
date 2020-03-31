360d45cfc142a613a9a834c5b23e37af0f570307
kifio <kg.of.morphine@gmail.com>
2020-03-31 22:26:08 +0200

    Add content for egg ViewController

---
+++ HelloWorld_osx/EggViewController.swift
@@ -0,0 +1,28 @@
+//
+//  EggViewController.swift
+//  HelloWorld_osx
+//
+//  Created by imurashov private on 31.03.20.
+//  Copyright © 2020 imurashov. All rights reserved.
+//
+
+import Cocoa
+
+class EggViewController: NSViewController {
+
+    @IBOutlet weak var timeLabel: NSTextField!
+    @IBOutlet weak var eggImage: NSImageView!
+
+    @IBOutlet weak var startButton: NSButton!
+    @IBOutlet weak var stopButton: NSButton!
+    @IBOutlet weak var resetButton: NSButton!
+    
+    @IBAction func startButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func stopButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func resetButtonClicked(_ sender: Any) {
+    }
+}
