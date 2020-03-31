f1cd25e3af104fb3ff39416026707e2421f7b337
kifio <kg.of.morphine@gmail.com>
2020-03-31 22:44:27 +0200

    add menu

--- HelloWorld_osx/AppDelegate.swift
+++ HelloWorld_osx/AppDelegate.swift
@@ -11,8 +11,12 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
+    @IBOutlet weak var startTimerMenuItem: NSMenuItem!
+    
+    @IBOutlet weak var stopTimerMenuItem: NSMenuItem!
+    
+    @IBOutlet weak var resetTimerMenuItem: NSMenuItem!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
